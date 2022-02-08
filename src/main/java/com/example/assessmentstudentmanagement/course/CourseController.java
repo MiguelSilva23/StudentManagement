96faf7dfe999e38a7f1f8c5f69ead21259d8ba14
AliceM80 <a.mutke@hotmail.de>
2022-02-08 14:18:19 +0100

    modify the course package

--- src/main/java/com/example/assessmentstudentmanagement/course/CourseController.java
+++ src/main/java/com/example/assessmentstudentmanagement/course/CourseController.java
@@ -1,7 +1,22 @@
 package com.example.assessmentstudentmanagement.course;
 
+import com.example.assessmentstudentmanagement.student.Student;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@AllArgsConstructor
 @Controller
 public class CourseController {
+
+  private CourseService courseService;
+
+  @GetMapping("/course list")
+  public String getCourseListPage(Model model){
+
+    model.addAttribute("regRequest", new Student());
+
+    return "course_list_page";
+  }
 }
